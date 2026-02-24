623f23cb744371b425d857f5b212640420668ff1
Gopinath <gopinath0332@outlook.com>
2026-02-24 15:48:30 +0530

    chore: Comment out restarts for multiple delta-bot services in `restart_bots.sh`.

--- scripts/restart_bots.sh
+++ scripts/restart_bots.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Restart Delta Bot Services
-echo "Restarting Service..."
-sudo systemctl restart delta-bot.service
+#echo "Restarting Service..."
+#sudo systemctl restart delta-bot.service
 
-echo "Restarting ETH Service..."
-sudo systemctl restart delta-bot-eth.service
+#echo "Restarting ETH Service..."
+#sudo systemctl restart delta-bot-eth.service
 
-echo "Restarting XRP Service..."
-sudo systemctl restart delta-bot-xrp.service
+#echo "Restarting XRP Service..."
+#sudo systemctl restart delta-bot-xrp.service
 
-echo "Restarting River Service..."
-sudo systemctl restart delta-bot-river.service
+#echo "Restarting River Service..."
+#sudo systemctl restart delta-bot-river.service
 
 echo "Restarting Pippin Service..."
 sudo systemctl restart delta-bot-pippin.service
 
-echo "Restarting BTC EMA Service..."
-sudo systemctl restart delta-bot-btc-ema.service
+#echo "Restarting BTC EMA Service..."
+#sudo systemctl restart delta-bot-btc-ema.service
 
 echo "Services restarted."
